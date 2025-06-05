9f5e761d0ef68dc2bf87fd2082876099f7b077fc
Becka <rweb198@wgu.edu>
2025-06-05 03:59:53 -0400

    B5.A complete- display detailed view of excursion

--- app/src/main/java/com/zybooks/myvacationplanner/UI/VacationDetails.java
+++ app/src/main/java/com/zybooks/myvacationplanner/UI/VacationDetails.java
@@ -1,6 +1,10 @@
 package com.zybooks.myvacationplanner.UI;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +12,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.zybooks.myvacationplanner.Database.Repository;
 import com.zybooks.myvacationplanner.R;
 
 public class VacationDetails extends AppCompatActivity {
+
+    String name;
+   int vacationID;
+   EditText editName;
+   EditText editPlace;
+   EditText editStartDate;
+   EditText editEndDate;
+   Repository repository;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +37,30 @@ public class VacationDetails extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        FloatingActionButton fab= findViewById(R.id.floatingActionButtonVacationDetails);
+        editName = findViewById(R.id.vacationtitletext);
+        editPlace = findViewById(R.id.placetostaytext);
+        editStartDate = findViewById(R.id.editTextStartDateVacations);
+        editEndDate = findViewById(R.id.editTextEndDateVacations);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(VacationDetails.this, ExcursionDetails.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    //menu inflater
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_vacation_details, menu);
+        return true;
+    }
+    // TODO: add save/delete vacation options for the vacation details menu
+
+
 }
