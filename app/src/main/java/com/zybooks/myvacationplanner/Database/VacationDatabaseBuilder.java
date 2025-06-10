32948183e04ffa66e6d2b57c8ea9bd77804f2625
Becka <rweb198@wgu.edu>
2025-06-09 23:13:24 -0400

    database incremented to clear test data

--- app/src/main/java/com/zybooks/myvacationplanner/Database/VacationDatabaseBuilder.java
+++ app/src/main/java/com/zybooks/myvacationplanner/Database/VacationDatabaseBuilder.java
@@ -11,7 +11,7 @@ import com.zybooks.myvacationplanner.DAO.VacationDAO;
 import com.zybooks.myvacationplanner.Entities.Excursion;
 import com.zybooks.myvacationplanner.Entities.Vacation;
 
-@Database(entities= {Vacation.class, Excursion.class}, version= 4, exportSchema = false)
+@Database(entities= {Vacation.class, Excursion.class}, version= 5, exportSchema = false)
 public abstract class VacationDatabaseBuilder extends RoomDatabase {
     public abstract VacationDAO vacationDAO();
     public abstract ExcursionDAO excursionDAO();
